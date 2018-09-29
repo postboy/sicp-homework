c7ad8a5cbe3ed96d2fe0c9aab19351d777e13bd1
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-09-29 13:01:07 +0300

    1.4, 1.5: minor changes - mark everything not suitable for unconditional execution as comments - 1.5: more comments

--- 1/5.scm
+++ 1/5.scm
@@ -1,4 +1,4 @@
-Exercise 1.5.  Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order evaluation or normal-order evaluation. He defines the following two procedures:
+; Exercise 1.5.  Ben Bitdiddle has invented a test to determine whether the interpreter he is faced with is using applicative-order evaluation or normal-order evaluation. He defines the following two procedures:
 
 (define (p) (p))
 
@@ -7,20 +7,22 @@ Exercise 1.5.  Ben Bitdiddle has invented a test to determine whether the interp
       0
       y))
 
-Then he evaluates the expression
+; Then he evaluates the expression
 
-(test 0 (p))
+; (test 0 (p))
 
-What behavior will Ben observe with an interpreter that uses applicative-order evaluation? What behavior will he observe with an interpreter that uses normal-order evaluation? Explain your answer. (Assume that the evaluation rule for the special form if is the same whether the interpreter is using normal or applicative order: The predicate expression is evaluated first, and the result determines whether to evaluate the consequent or the alternative expression.) 
+; What behavior will Ben observe with an interpreter that uses applicative-order evaluation? What behavior will he observe with an interpreter that uses normal-order evaluation? Explain your answer. (Assume that the evaluation rule for the special form if is the same whether the interpreter is using normal or applicative order: The predicate expression is evaluated first, and the result determines whether to evaluate the consequent or the alternative expression.) 
 
-Applicative-order evaluation:
-(test 0 (p))
-(test 0 (p))
-...
+; Applicative-order evaluation:
+; (test 0 (p))
+; (test 0 (p))
+; ...
+; Evaluation is running in endless cycle.
 
-Normal-order evaluation:
-(if (= 0 0)
-      0
-      (p))
-(0)
-0
+; Normal-order evaluation:
+; (if (= 0 0)
+;      0
+;      (p))
+; (0)
+; 0
+; Evaluation yields an answer.
