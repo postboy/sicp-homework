2da1b48f84b2183f90b9741219e7d1ecd1ca73e0
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-10-13 16:18:06 +0300

    nanofixes 1.14: marked comments as such, used assert, added comment 1.3: loaded file with assert

--- 1/3.scm
+++ 1/3.scm
@@ -1,5 +1,7 @@
 ; Exercise 1.3.  Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 
+(load "ch1_common.scm")
+
 (define (square x) (* x x))
 
 (define (sum-of-squares x y)
