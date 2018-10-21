6662422aedd1f1ddc3220cf68cd28b90c0b214f8
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-10-21 22:15:22 +0300

    1.11: add tests

--- 1/11.scm
+++ 1/11.scm
@@ -1,12 +1,14 @@
-Exercise 1.11.  A function f is defined by the rule that f(n) = n if n < 3 and f(n) = f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process.
+; Exercise 1.11.  A function f is defined by the rule that f(n) = n if n < 3 and f(n) = f(n-1) + 2f(n-2) + 3f(n-3) if n >= 3. Write a procedure that computes f by means of a recursive process. Write a procedure that computes f by means of an iterative process.
 
-(define (f n)
+(load "ch1_common.scm")
+
+(define (f1 n)
   (cond ((< n 3) n)
-        (else (+ (f (- n 1))
-                 (* 2 (f (- n 2)))
-		 (* 3 (f (- n 3)))))))
+        (else (+ (f1 (- n 1))
+                 (* 2 (f1 (- n 2)))
+		 (* 3 (f1 (- n 3)))))))
 
-(define (f n)
+(define (f2 n)
   (f-iter 2 1 0 n))
 
 (define (f-iter a b c count)
@@ -19,17 +21,14 @@ Exercise 1.11.  A function f is defined by the rule that f(n) = n if n < 3 and f
 		      b
 		      (- count 1)))))
 
-(f -1)
-; -1
-(f 0)
-; 0
-(f 1)
-; 1
-(f 2)
-; 2
-(f 3)
-; 4
-(f 4)
-; 11
-(f 5)
-; 25
+(define (test-f f)
+  (assert (f -1) -1)
+  (assert (f 0) 0)
+  (assert (f 1) 1)
+  (assert (f 2) 2)
+  (assert (f 3) 4)
+  (assert (f 4) 11)
+  (assert (f 5) 25))
+
+(test-f f1)
+(test-f f2)
