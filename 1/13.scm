f52e1a94d23313b9ac925e282be23d395e14bd09
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-10-27 16:01:55 +0300

    1.13, 1.15, 1.16, 1.17: tests, comments

--- 1/13.scm
+++ 1/13.scm
@@ -1,6 +1,6 @@
-Exercise 1.13.  Prove that Fib(n) is the closest integer to a^n/5, where a = (1 + sqrt(5))/2. Hint: Let b = (1 - sqrt(5))/2. Use induction and the definition of the Fibonacci numbers (see section 1.2.2) to prove that Fib(n) = (a^n - b^n)/sqrt(5).
+; Exercise 1.13.  Prove that Fib(n) is the closest integer to a^n/5, where a = (1 + sqrt(5))/2. Hint: Let b = (1 - sqrt(5))/2. Use induction and the definition of the Fibonacci numbers (see section 1.2.2) to prove that Fib(n) = (a^n - b^n)/sqrt(5).
 
-TODO: unsolved
+; TODO: unsolved
 
-f(n) = (a^n - b^n)/sqrt(5)
-f(n) = ( ( (1 + sqrt(5))/2 )^n - ( (1 - sqrt(5) )/2 )^n ) / sqrt(5)
+; f(n) = (a^n - b^n)/sqrt(5)
+; f(n) = ( ( (1 + sqrt(5))/2 )^n - ( (1 - sqrt(5) )/2 )^n ) / sqrt(5)
--- 1/17.scm
+++ 1/17.scm
@@ -1,11 +1,13 @@
-Exercise 1.17.  The exponentiation algorithms in this section are based on performing exponentiation by means of repeated multiplication. In a similar way, one can perform integer multiplication by means of repeated addition. The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the expt procedure:
+; Exercise 1.17.  The exponentiation algorithms in this section are based on performing exponentiation by means of repeated multiplication. In a similar way, one can perform integer multiplication by means of repeated addition. The following multiplication procedure (in which it is assumed that our language can only add, not multiply) is analogous to the expt procedure:
+
+(load "ch1_common.scm")
 
 (define (* a b)
   (if (= b 0)
       0
       (+ a (* a (- b 1)))))
 
-This algorithm takes a number of steps that is linear in b. Now suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. Using these, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps.
+; This algorithm takes a number of steps that is linear in b. Now suppose we include, together with addition, operations double, which doubles an integer, and halve, which divides an (even) integer by 2. Using these, design a multiplication procedure analogous to fast-expt that uses a logarithmic number of steps.
 
 (define (double n)
   (* n 2))
@@ -21,19 +23,19 @@ This algorithm takes a number of steps that is linear in b. Now suppose we inclu
         ((even? b) (double (fast-mul a (halve b))))
         (else (+ a (fast-mul a (- b 1))))))
 
-(fast-mul 0 0) ; 0
-(fast-mul 0 1) ; 0
-(fast-mul 1 0) ; 0
-(fast-mul 1 1) ; 1
-(fast-mul 1 2) ; 2
-(fast-mul 1 3) ; 3
-(fast-mul 2 0) ; 0
-(fast-mul 2 1) ; 2
-(fast-mul 2 2) ; 4
-(fast-mul 2 3) ; 6
-(fast-mul 3 0) ; 0
-(fast-mul 3 1) ; 3
-(fast-mul 3 2) ; 6
-(fast-mul 3 3) ; 9
-(fast-mul 3 4) ; 12
-(fast-mul 3 5) ; 15
+(assert 0 (fast-mul 0 0))
+(assert 0 (fast-mul 0 1))
+(assert 0 (fast-mul 1 0))
+(assert 1 (fast-mul 1 1))
+(assert 2 (fast-mul 1 2))
+(assert 3 (fast-mul 1 3))
+(assert 0 (fast-mul 2 0))
+(assert 2 (fast-mul 2 1))
+(assert 4 (fast-mul 2 2))
+(assert 6 (fast-mul 2 3))
+(assert 0 (fast-mul 3 0))
+(assert 3 (fast-mul 3 1))
+(assert 6 (fast-mul 3 2))
+(assert 9 (fast-mul 3 3))
+(assert 12 (fast-mul 3 4))
+(assert 15 (fast-mul 3 5))
