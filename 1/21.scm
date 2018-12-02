6d3de6f2e256d8ed66d0c610ee7792fb2c224521
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-12-02 12:26:39 +0300

    1.21: slightly more general algorithm for shorter 1.23 1.22: minor changes and re-collected statistics

--- 1/21.scm
+++ 1/21.scm
@@ -5,10 +5,13 @@
 (define (divides? a b)
   (= (remainder b a) 0))
 
+(define (next n)
+  (+ n 1))
+
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 
 (define (smallest-divisor n)
   (find-divisor n 2))
