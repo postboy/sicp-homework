80e63d27cff4258b5b7fb914045d91617d50c63e
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-12-28 09:36:55 +0300

    1.40

---
+++ 13/40.scm
@@ -0,0 +1,10 @@
+; Exercise 1.40.  Define a procedure cubic that can be used together with the newtons-method procedure in expressions of the form <...> to approximate zeros of the cubic x^3 + a*x^2 + b*x + c.
+
+(load "ch13_common.scm")
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x) (* (square x) a) (* b x) c)))
+
+(newtons-method (cubic 1 1 -3) 1)    ; actual: 1, expected: 1
+(newtons-method (cubic 1 1 1) 1)     ; -0.9999999999997796, -1
+(newtons-method (cubic 10 20 30) 1)  ; -7.961132876317207, -7.9611
--- 13/ch13_common.scm
+++ 13/ch13_common.scm
@@ -54,8 +54,24 @@
           (try next (inc i)))))
   (try first-guess 1))
 
-(define (fixed-pointf first-guess)
+(define (fixed-point f first-guess)
   (fixed-point-internal f first-guess #f))
 
 (define (fixed-point-dbg f first-guess)
   (fixed-point-internal f first-guess #t))
+
+; newtons-method
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
