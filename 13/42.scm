f427d8c8676a1484579325127474c38a0018f88f
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-12-28 10:38:14 +0300

    1.42

---
+++ 13/42.scm
@@ -0,0 +1,7 @@
+; Exercise 1.42.  Let f and g be two one-argument functions. The composition f after g is defined to be the function x -> f(g(x)). Define a procedure compose that implements composition. For example, if inc is a procedure that adds 1 to its argument,
+
+(load "ch13_common.scm")
+
+(define (compose f g) (lambda (x) (f (g x))))
+
+(assert 49 ((compose square inc) 6))
