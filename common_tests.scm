642a678576c27821f7936b6cf872c4ff2645fcb3
Ivan Zuboff <anotherdiskmag@gmail.com>
2018-12-30 14:39:58 +0300

    small fixes in 12, more tests on generalized assert

--- common_tests.scm
+++ common_tests.scm
@@ -2,7 +2,11 @@
 
 (assert #t (assert 0 0))
 (assert #t (assert 'a 'a))
+(assert #t (assert 1/4 1/4))
+(assert #t (assert 0.1 0.1))
 ; following lines cause error messages on console
 (assert #f (assert 0 1))
 (assert #f (assert 'a 'b))
 (assert #f (assert 0 'a))
+(assert #f (assert 0 0.1))
+(assert #f (assert 0 1/4))
