236e384273bbcf119bb97610551d0bd06058d397
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-05-03 11:47:45 +0300

    2.22; small fixes in 2.21

--- 22/21.scm
+++ 22/21.scm
@@ -11,11 +11,11 @@
 (define (square-list-1 items)
   (if (null? items)
       nil
-      (cons (* (car items) (car items))
-	    (square-list (cdr items)))))
+      (cons (square (car items))
+	    (square-list-1 (cdr items)))))
 
 (define (square-list-2 items)
-  (map (lambda (x) (* x x)) items))
+  (map square items))
 
 (assert (list 1 4 9 16) (square-list-1 (list 1 2 3 4)))
 (assert (list 1 4 9 16) (square-list-2 (list 1 2 3 4)))
---
+++ 22/22.scm
@@ -0,0 +1,32 @@
+; Exercise 2.22.  Louis Reasoner tries to rewrite the first square-list procedure of exercise 2.21 so that it evolves an iterative process. Unfortunately, defining square-list this way produces the answer list in the reverse order of the one desired. Why?
+
+(load "ch22_common.scm")
+
+(define (square-list-3 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things) 
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+; This function adds new square to the list before (instead of after) previous squares.
+
+; Louis then tries to fix his bug by interchanging the arguments to cons. This doesn't work either. Explain.
+
+(define (square-list-4 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items nil))
+
+; Here pairs are consed in wrong order, reverse to pair order in list:
+; (list <a1> <a2> ... <an>) = (cons <a1> (cons <a2> (cons ... (cons <an> nil) ...)))
+; Also, nil is not on the end of list, where it should be.
+
+(assert (list 16 9 4 1) (square-list-3 (list 1 2 3 4)))
+(assert (cons (cons (cons (cons nil 1) 4) 9) 16) (square-list-4 (list 1 2 3 4)))
