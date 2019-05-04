a8010dea9c45495947cd7ac8e6981ab93f712cfd
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-05-04 13:18:03 +0300

    2.30

---
+++ 22/30.scm
@@ -0,0 +1,20 @@
+; Exercise 2.30.  Define a procedure square-tree analogous to the square-list procedure of exercise 2.21. Define square-tree both directly (i.e., without using any higher-order procedures) and also by using map and recursion.
+
+(load "ch22_common.scm")
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (square tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(assert (square-tree '(1 (2 (3 4) 5) (6 7))) '(1 (4 (9 16) 25) (36 49)))
+
+(define (scale-tree tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
+
+(assert (square-tree '(1 (2 (3 4) 5) (6 7))) '(1 (4 (9 16) 25) (36 49)))
