fa1e319e922293eb714ebb2385ffff1f963802b4
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-05-19 16:05:47 +0300

    2.53

---
+++ 22/53.scm
@@ -0,0 +1,22 @@
+; Exercise 2.53.  What would the interpreter print in response to evaluating each of the following expressions?
+
+(load "ch22_common.scm")
+
+(define (memq item x)
+  (cond ((null? x) false)
+        ((eq? item (car x)) x)
+        (else (memq item (cdr x)))))
+
+(assert '(a b c) (list 'a 'b 'c))
+
+(assert '((george)) (list (list 'george)))
+
+(assert '((y1 y2)) (cdr '((x1 x2) (y1 y2))))
+
+(assert '(y1 y2) (cadr '((x1 x2) (y1 y2))))
+
+(assert #f (pair? (car '(a short list))))
+
+(assert #f (memq 'red '((red shoes) (blue socks))))
+
+(assert '(red shoes blue socks) (memq 'red '(red shoes blue socks)))
