721b9ccfe357873dd2630e319b1f35447feb1960
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-05-26 12:27:06 +0300

    2.56

---
+++ 22/56.scm
@@ -0,0 +1,53 @@
+; Exercise 2.56.  Show how to extend the basic differentiator to handle more kinds of expressions. For instance, implement the differentiation rule <...> by adding a new clause to the deriv program and defining appropriate procedures exponentiation?, base, exponent, and make-exponentiation. (You may use the symbol ** to denote exponentiation.) Build in the rules that anything raised to the power 0 is 1 and anything raised to the power 1 is the thing itself.
+
+(load "ch22_common.scm")
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base p) (cadr p))
+
+(define (exponent p) (caddr p))
+
+(define (make-exponentiation base exp)
+  (cond ((=number? base 0) 0)
+        ((=number? base 1) 1)
+	((=number? exp 0) 1)
+	((=number? exp 1) base)
+        ((and (number? base) (number? exp)) (expt base exp))
+        (else (list '** base exp))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+	((exponentiation? exp)
+	 (make-product
+	  (make-product (exponent exp)
+			(make-exponentiation (base exp)
+					     (- (exponent exp) 1)))
+	  (deriv (base exp) var)))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(assert (deriv '(** x 5) 'x) '(* 5 (** x 4)))
+(assert (deriv '(** 5 5) 'x) 0)
+(assert (deriv '(** x 5) 'y) 0)
+(assert (deriv '(** x 0) 'x) 0)
+(assert (deriv '(** x 1) 'x) 1)
+
+; answer is definitely not properly simplified, but it's right
+(assert (deriv '(* (** x 1) (** x 1)) 'x) '(+ (** x 1) (** x 1)))
+
+; this expressions require more complex logic in deriv:
+; (assert (deriv '(** 0 x) 'x) 0)
+; (assert (deriv '(** 1 x) 'x) 1)
--- 22/ch22_common.scm
+++ 22/ch22_common.scm
@@ -99,3 +99,38 @@
     (let ((top (beside (tl painter) (tr painter)))
           (bottom (beside (bl painter) (br painter))))
       (below bottom top))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s) (caddr s))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p) (caddr p))
