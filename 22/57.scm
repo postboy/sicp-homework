c7dffbea0da55a0ff3c61b2e6d25327b0260de47
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-05-26 13:24:37 +0300

    2.57

---
+++ 22/57.scm
@@ -0,0 +1,21 @@
+; Exercise 2.57.  Extend the differentiation program to handle sums and products of arbitrary numbers of (two or more) terms. <...> Try to do this by changing only the representation for sums and products, without changing the deriv procedure at all. For example, the addend of a sum would be the first term, and the augend would be the sum of the rest of the terms.
+
+(load "ch22_common.scm")
+
+(define (augend l)
+  (define (sum-of l)
+    (if (null? (cdr l))
+	(car l)
+	(make-sum (car l) (sum-of (cdr l)))))
+  (sum-of (cddr l)))
+
+(define (multiplicand l)
+  (define (product-of l)
+    (if (null? (cdr l))
+	(car l)
+	(make-product (car l) (product-of (cdr l)))))
+  (product-of (cddr l)))
+
+(assert (deriv '(+ x x x) 'x) 3)
+(assert (deriv '(* x x x) 'x) '(+ (* x (+ x x)) (* x x)))
+(assert (deriv '(* x y (+ x 3 1)) 'x) '(+ (* x y) (* y (+ x 3 1))))
--- 22/ch22_common.scm
+++ 22/ch22_common.scm
@@ -134,3 +134,19 @@
 (define (multiplier p) (cadr p))
 
 (define (multiplicand p) (caddr p))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
