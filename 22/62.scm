18289614f2f31d0ca5960e623884dcd2686b4adc
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-06-02 15:15:00 +0300

    2.62

---
+++ 22/62.scm
@@ -0,0 +1,25 @@
+; Exercise 2.62.  Give a O(n) implementation of union-set for sets represented as ordered lists.
+
+(load "ch22_common.scm")
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1)
+	(else (let ((x1 (car set1)) (x2 (car set2)))
+		(cond ((= x1 x2)
+		       (cons x1
+			     (union-set (cdr set1)
+					(cdr set2))))
+		      ((< x1 x2)
+		       (cons x1
+			     (union-set (cdr set1) set2)))
+		      ((< x2 x1)
+		       (cons x2
+			     (union-set set1 (cdr set2)))))))))
+
+(assert (union-set '() '()) '())
+(assert (union-set '() '(1)) '(1))
+(assert (union-set '(1) '()) '(1))
+(assert (union-set '(1) '(1)) '(1))
+(assert (union-set '(1) '(2)) '(1 2))
+(assert (union-set '(2) '(1)) '(1 2))
