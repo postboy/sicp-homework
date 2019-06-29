4cda23889cfa3ffd13617089c0ebf0556806c09d
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-06-29 17:36:47 +0300

    2.73 finished

--- 24/73.scm
+++ 24/73.scm
@@ -1,5 +1,8 @@
 ; Exercise 2.73.  Section 2.3.2 described a program that performs symbolic differentiation:
 
+(cd "../23")
+(load "56.scm")
+(cd "../24")
 (load "ch24_common.scm")
 
 (define (deriv exp var)
@@ -20,13 +23,11 @@
 ; We can regard this program as performing a dispatch on the type of the expression to be differentiated. In this situation the ``type tag'' of the datum is the algebraic operator symbol (such as +) and the operation being performed is deriv. We can transform this program into data-directed style by rewriting the basic derivative procedure as
 
 (define (deriv exp var)
-   (cond ((number? exp) 0)
-         ((variable? exp) (if (same-variable? exp var) 1 0))
-         (else ((get 'deriv (operator exp)) (operands exp) var))))
-
-(define (operator exp) (car exp))
-
-(define (operands exp) (cdr exp))
+  (define (operator exp) (car exp))
+  (define (operands exp) (cdr exp))
+  (cond ((number? exp) 0)
+	((variable? exp) (if (same-variable? exp var) 1 0))
+	(else ((get 'deriv (operator exp)) (operands exp) var))))
 
 ; a.  Explain what was done above. Why can't we assimilate the predicates number? and same-variable? into the data-directed dispatch?
 
@@ -36,13 +37,78 @@
 
 ; b.  Write the procedures for derivatives of sums and products, and the auxiliary code required to install them in the table used by the program above.
 
+; Let's make the simplest implementation where all expressions have only two operands.
 
+(define (sum-derive operands var)
+  (define addend (car operands))
+  (define augend (cadr operands))
+  (make-sum (deriv addend var)
+	    (deriv augend var)))
+
+(define (product-derive operands var)
+  (define multiplier (car operands))
+  (define multiplicand (cadr operands))
+  (make-sum
+   (make-product multiplier
+		 (deriv multiplicand var))
+   (make-product (deriv multiplier var)
+		 multiplicand)))
+
+(put 'deriv '+ sum-derive)
+(put 'deriv '* product-derive)
+
+(assert (get 'deriv '+) sum-derive)
+(assert (get 'deriv '*) product-derive)
+
+(assert (deriv '(+ x 3) 'x) 1)
+(assert (deriv '(* x y) 'x) 'y)
+(assert (deriv '(* (* x y) (+ x 3)) 'x) '(+ (* x y) (* y (+  x 3))))
 
 ; c.  Choose any additional differentiation rule that you like, such as the one for exponents (exercise 2.56), and install it in this data-directed system.
 
+(define (exponentiation-derive operands var)
+  (define base (car operands))
+  (define exponent (cadr operands))
+  (make-product
+   (make-product exponent
+		 (make-exponentiation base
+				      (- exponent 1)))
+   (deriv base var)))
 
+(put 'deriv '** exponentiation-derive)
+(assert (get 'deriv '**) exponentiation-derive)
 
-; d.  In this simple algebraic manipulator the type of an expression is the algebraic operator that binds it together. Suppose, however, we indexed the procedures in the opposite way, so that the dispatch line in deriv looked like <...>
-; What corresponding changes to the derivative system are required?
+(assert (deriv '(** x 5) 'x) '(* 5 (** x 4)))
+(assert (deriv '(** 5 5) 'x) 0)
+(assert (deriv '(** x 5) 'y) 0)
+(assert (deriv '(** x 0) 'x) 0)
+(assert (deriv '(** x 1) 'x) 1)
+(assert (deriv '(* (** x 1) (** x 1)) 'x) '(+ (** x 1) (** x 1)))
 
-; ((get (operator exp) 'deriv) (operands exp) var)
+; d.  In this simple algebraic manipulator the type of an expression is the algebraic operator that binds it together. Suppose, however, we indexed the procedures in the opposite way, so that the dispatch line in deriv looked like ((get (operator exp) 'deriv) (operands exp) var). What corresponding changes to the derivative system are required?
+
+(define (deriv exp var)
+  (define (operator exp) (car exp))
+  (define (operands exp) (cdr exp))
+   (cond ((number? exp) 0)
+         ((variable? exp) (if (same-variable? exp var) 1 0))
+         (else ((get (operator exp) 'deriv) (operands exp) var))))
+
+(put '+ 'deriv sum-derive)
+(put '* 'deriv product-derive)
+(put '** 'deriv exponentiation-derive)
+
+(assert (get '+ 'deriv) sum-derive)
+(assert (get '* 'deriv) product-derive)
+(assert (get '** 'deriv) exponentiation-derive)
+
+(assert (deriv '(+ x 3) 'x) 1)
+(assert (deriv '(* x y) 'x) 'y)
+(assert (deriv '(* (* x y) (+ x 3)) 'x) '(+ (* x y) (* y (+  x 3))))
+
+(assert (deriv '(** x 5) 'x) '(* 5 (** x 4)))
+(assert (deriv '(** 5 5) 'x) 0)
+(assert (deriv '(** x 5) 'y) 0)
+(assert (deriv '(** x 0) 'x) 0)
+(assert (deriv '(** x 1) 'x) 1)
+(assert (deriv '(* (** x 1) (** x 1)) 'x) '(+ (** x 1) (** x 1)))
--- 24/ch24_common.scm
+++ 24/ch24_common.scm
@@ -1,1 +1,9 @@
 (load "../common.scm")
+
+(define *op-table* (make-hash-table))
+
+(define (put op type proc)
+  (hash-table/put! *op-table* (list op type) proc))
+
+(define (get op type)
+  (hash-table/get *op-table* (list op type) #f))
