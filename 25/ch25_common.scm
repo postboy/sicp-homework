35a164bd6d050911db5708b5d8241cd0bc54e5eb
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-08-28 17:08:31 +0300

    ch25_common.scm: add rational numbers support
    prepare for 2.79 and 2.80

--- 25/ch25_common.scm
+++ 25/ch25_common.scm
@@ -50,6 +50,44 @@
 (define (make-scheme-number n)
   ((get 'make 'scheme-number) n))
 
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
 (define (install-rectangular-package)
   ;; internal procedures
   (define (real-part z) (car z))
@@ -115,5 +153,6 @@
   ((get 'make-from-mag-ang 'complex) r a))
 
 (install-scheme-number-package)
+(install-rational-package)
 (install-rectangular-package)
 (install-complex-package)
