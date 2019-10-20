ace78c9a3d5c38fd3dcb02f6dc033e73aee599e9
Ivan Zuboff <anotherdiskmag@gmail.com>
2019-10-20 10:26:42 +0300

    2.78: move solution to ch25_common.scm
    I suppose that it will simplify solutions of 2.86-2.97.
    Also, fix attach-tag implementation so it doesn't break 2.83 where tags
    like "integer" should be attached despite numbers aren't pairs.

--- 25/78.scm
+++ 25/78.scm
@@ -1,21 +1,7 @@
 ; Exercise 2.78.  The internal procedures in the scheme-number package are essentially nothing more than calls to the primitive procedures +, -, etc. It was not possible to use the primitives of the language directly because our type-tag system requires that each data object have a type attached to it. In fact, however, all Lisp implementations do have a type system, which they use internally. Primitive predicates such as symbol? and number? determine whether data objects have particular types. Modify the definitions of type-tag, contents, and attach-tag from section 2.4.2 so that our generic system takes advantage of Scheme's internal type system. That is to say, the system should work as before except that ordinary numbers should be represented simply as Scheme numbers rather than as pairs whose car is the symbol scheme-number.
 
+; Solution is there:
 (load "ch25_common.scm")
-
-(define (attach-tag type-tag contents)
-  (if (pair? contents)
-      (cons type-tag contents)
-      contents))
-
-(define (type-tag datum)
-  (cond ((pair? datum) (car datum))
-	((number? datum) 'scheme-number)
-	(else (error "Bad tagged datum -- TYPE-TAG" datum))))
-
-(define (contents datum)
-  (cond ((pair? datum) (cdr datum))
-	((number? datum) datum)
-	(else (error "Bad tagged datum -- CONTENTS" datum))))
 
 (assert (make-scheme-number 3) 3)
 (assert (add 3 3) 6)
--- 25/ch25_common.scm
+++ 25/ch25_common.scm
@@ -1,17 +1,19 @@
 (load "../common.scm")
 
 (define (attach-tag type-tag contents)
-  (cons type-tag contents))
+  (if (eq? type-tag 'scheme-number)
+      contents
+      (cons type-tag contents)))
 
 (define (type-tag datum)
-  (if (pair? datum)
-      (car datum)
-      (error "Bad tagged datum -- TYPE-TAG" datum)))
+  (cond ((pair? datum) (car datum))
+	((number? datum) 'scheme-number)
+	(else (error "Bad tagged datum -- TYPE-TAG" datum))))
 
 (define (contents datum)
-  (if (pair? datum)
-      (cdr datum)
-      (error "Bad tagged datum -- CONTENTS" datum)))
+  (cond ((pair? datum) (cdr datum))
+	((number? datum) datum)
+	(else (error "Bad tagged datum -- CONTENTS" datum))))
 
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
