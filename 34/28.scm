fad01a44cb93a77457af0f0539e08331a7533338
Ivan Zuboff <anotherdiskmag@gmail.com>
2020-04-11 14:33:29 +0300

    3.28 draft

---
+++ 34/28.scm
@@ -0,0 +1,23 @@
+; Exercise 3.28.  Define an or-gate as a primitive function box. Your or-gate constructor should be similar to and-gate.
+
+(load "ch34_common.scm")
+
+(define (logical-or a1 a2)
+  (cond ((and (= a1 0) (= a2 0)) 0)
+	((and (= a1 0) (= a2 1)) 1)
+	((and (= a1 1) (= a2 0)) 1)
+	((and (= a1 1) (= a2 1)) 1)
+	(else (error "Invalid signal(s)" a1 a2))))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+; tests here
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Structure and Interpretation of Computer Programs, everyone!
 
 Solved chapters: 1, 2, 3 (partly)
+TODO: 3: 26, 28
 
 Licence: public domain
