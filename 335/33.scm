b5d8a6c8af0db26defe860aead3b0c02099a2dd6
Ivan Zuboff <anotherdiskmag@gmail.com>
2020-04-25 16:19:42 +0300

    3.33

---
+++ 335/33.scm
@@ -0,0 +1,34 @@
+; Exercise 3.33.  Using primitive multiplier, adder, and constant constraints, define a procedure averager that takes three connectors a, b, and c as inputs and establishes the constraint that the value of c is the average of the values of a and b.
+
+(load "ch335_common.scm")
+
+(define (averager a b c)
+  (let ((a-m (make-connector))
+        (half (make-connector)))
+    (adder a b a-m)
+    (multiplier a-m half c)
+    (constant 0.5 half)
+    'ok))
+
+(define first (make-connector))
+(probe 'first first)
+(define second (make-connector))
+(probe 'second second)
+(define average (make-connector))
+(probe 'average average)
+
+(averager first second average)
+
+(assert (set! past-events nil) nil)
+(set-value! first 10 'user)
+(set-value! second 20 'user)
+(assert (set! past-events nil) '((second 20) (average 15.) (first 10)))
+
+(forget-value! second 'user)
+(assert (set! past-events nil) '((second "?") (average "?")))
+
+(set-value! average 15 'user)
+(assert (set! past-events nil) '((average 15) (second 20.)))
+
+(set-value! second 20 'user)
+(assert (set! past-events nil) nil)
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Solved chapters: 1, 2, 3 (partly)
 
-TODO: 3: 26
+TODO: 3: 26 32
 
 Licence: public domain
